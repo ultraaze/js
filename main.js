e9d2020c0f054f1e3b6c48e389f6f913609df8e9
Rafa <idchrom@gmail.com>
2019-04-30 14:01:37 +0300

    this is my first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+var rafa;
+console.log(rafa);
+
+var person = {
+	name: "rafa",
+	surname: "dio"
+};
+
+//alert(person.name + " " + person.surname);
+
+// prompt('dddd', 'default');
+
+console.log(typeof(person));
+
+var y =10;
+y++;
+console.log(10 ===10);
+
+console.log(3 + 4 * 5); // 3 + 20
